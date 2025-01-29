5821b51ccc66bdbf0e7552de82df5be8545e0134
Antonella Denisse Cuenca <fuwa08@hotmail.com>
2025-01-29 13:34:07 -0300

    Add projects data structure and update .gitignore

---
+++ mi-portfolio/src/info_proyectos/projects.js
@@ -0,0 +1,67 @@
+const projects = [
+  {
+    id: 1,
+    name: "Plataforma Web CIDEPINT",
+    description:
+      "Plataforma web para CIDEPINT con arquitecturas tanto de microservicios como monolítica para comparar rendimiento y escalabilidad.",
+    tech: [
+      {
+        name: "Flask",
+        image: "src/assets/flask.png",
+        url: "https://flask.palletsprojects.com/en/stable/",
+      },
+      {
+        name: "Vuejs",
+        image: "src/assets/vuejs.png",
+        url: "https://vuejs.org/",
+      },
+      {
+        name: "postgresql",
+        image: "src/assets/postgresql.png",
+        url: "https://www.postgresql.org/",
+      },
+    ],
+    link: "https://grupo13.proyecto2023.linti.unlp.edu.ar/",
+  },
+  {
+    id: 2,
+    name: "Proyectos personales",
+    description:
+      "Una selección de proyectos personales que demuestran mis habilidades y experiencia, disponibles en mi GitHub.",
+    tech: [
+      {
+        name: "Github",
+        image: "src/assets/github.png",
+        url: "https://github.com/",
+      },
+      {
+        name: "Python",
+        image: "src/assets/python.png",
+        url: "https://www.python.org/",
+      },
+      {
+        name: "Java",
+        image: "src/assets/java.png",
+        url: "https://www.java.com/",
+      },
+      {
+        name: "JavaScript",
+        image: "src/assets/javascript.png",
+        url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+      },
+      {
+        name: "HTML",
+        image: "src/assets/html.png",
+        url: "https://developer.mozilla.org/en-US/docs/Web/HTML",
+      },
+      {
+        name: "CSS",
+        image: "src/assets/css.png",
+        url: "https://developer.mozilla.org/en-US/docs/Web/CSS",
+      },
+    ],
+    link: "https://github.com/Fuwanto",
+  },
+];
+
+export default projects;
